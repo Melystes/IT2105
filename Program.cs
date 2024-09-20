5bcc2e5b74f6175e8de0d2b1cbe5e0e361eda6ee
Melystes <202310040@feucavite.edu.ph>
2024-09-20 10:50:17 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT2105_Seatwork1_LOYOLA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eng, math, sci, mapeh;
+            double sum, avg, engdouble, mathdouble, scidouble, mapehdouble;
+
+            Console.Write("Please input student's grade in English: ");
+            string english = Console.ReadLine();
+
+            Console.Write("Please input student's grade in Math: ");
+            string mth = Console.ReadLine();
+
+            Console.Write("Please input student's grade in Science: ");
+            string science = Console.ReadLine();
+
+            Console.Write("Please input student's grade in MAPEH: ");
+            string mape = Console.ReadLine();
+
+            eng = Convert.ToInt32(english);
+            math = Convert.ToInt32(mth);
+            sci = Convert.ToInt32(science);
+            mapeh = Convert.ToInt32(mape);
+
+            engdouble = Convert.ToDouble(eng);
+            mathdouble = Convert.ToDouble(math);
+            scidouble = Convert.ToDouble(sci);
+            mapehdouble = Convert.ToDouble(mapeh);
+
+
+            sum = engdouble + mathdouble + scidouble + mapehdouble;
+            avg = sum / 4;
+
+            Console.WriteLine("Average: " + avg);
+
+            if(avg == 100 && avg >= 98)
+            {
+                Console.WriteLine("With Highest Honors");
+            }
+            else if(avg >= 95 && avg < 97.99)
+            {
+                Console.WriteLine("With High Honors");
+            }
+            else if (avg >= 90 && avg < 94.99)
+            {
+                Console.WriteLine("With Honors");
+            }
+            else if (avg >= 75 && avg < 89.99)
+            {
+                Console.WriteLine("Passed");
+            }
+            else if (avg < 75)
+            {
+                Console.WriteLine("Failed");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
